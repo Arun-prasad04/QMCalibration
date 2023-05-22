2733018a0893647576402685b4e47d62e5e12d20
Karthick-392 <karthick.s@quantrium.ai>
2023-05-22 14:34:16 +0530

    Master changes by karthick

--- WEB/Controllers/HomeController.cs
+++ WEB/Controllers/HomeController.cs
@@ -13,109 +13,112 @@ namespace WEB.Controllers;
 
 public class HomeController : BaseController
 {
-    private readonly IMapper _mapper;
+	private readonly IMapper _mapper;
 
-    private CMTDatabaseContext db;
-    private IUnitOfWork _unitOfWork { get; set; }
-    private IMasterService _masterService{get;set;}
+	private CMTDatabaseContext db;
+	private IUnitOfWork _unitOfWork { get; set; }
+	private IMasterService _masterService { get; set; }
 
 	private IConfiguration _configuration;
-	public HomeController(IUnitOfWork unitOfWork, IMapper mapper,IMasterService masterService,ILogger<BaseController>logger,IHttpContextAccessor contextAccessor, IConfiguration Configuration) :base(logger,contextAccessor)
-    {
-        _masterService=masterService;
-        _unitOfWork=unitOfWork;
-        _mapper = mapper;
+	public HomeController(IUnitOfWork unitOfWork, IMapper mapper, IMasterService masterService, ILogger<BaseController> logger, IHttpContextAccessor contextAccessor, IConfiguration Configuration) : base(logger, contextAccessor)
+	{
+		_masterService = masterService;
+		_unitOfWork = unitOfWork;
+		_mapper = mapper;
 		_configuration = Configuration;
 	}
 
-    public IActionResult Index()
-    {
-        ViewBag.PageTitle="DashBoard";
-        int userId = Convert.ToInt32(base.SessionGetString("LoggedId"));
-        int userRoleId = Convert.ToInt32(base.SessionGetString("UserRoleId"));
-        UserViewModel labUserById = _mapper.Map<UserViewModel>(_unitOfWork.Repository<User>().GetQueryAsNoTracking(Q => Q.Id == userId).SingleOrDefault());
-        List<Master> MasterList = _unitOfWork.Repository<Master>().GetQueryAsNoTracking(g=>g.QuarantineModel.Select(s=>s.StatusId).FirstOrDefault()==2).ToList();
-        if(MasterList!=null)
-        {
-            ViewBag.MasterCount=MasterList.Count;
-        }
-        else
-        {
-            ViewBag.MasterCount=0;
-        }
-        if(userRoleId==2 || userRoleId==4)
-        {
-            List<Instrument> InstrumentList = _unitOfWork.Repository<Instrument>().GetQueryAsNoTracking(Q => Q.QuarantineModel.Select(s => s.StatusId).FirstOrDefault() == 2 && (Q.IdNo!="" && Q.IdNo!=null)).Include(I => I.QuarantineModel).Include(I => I.FileUploadModel).Include(I => I.RequestModel).Include(G => G.DepartmenttModel).ToList();
-            if(InstrumentList!=null)
-            {
-                ViewBag.InstrumentCount=InstrumentList.Count;
-            }
-            else
-            {
-                ViewBag.InstrumentCount=0;
-            }
-        }
-        else{
-            List<Instrument> InstrumentList = _unitOfWork.Repository<Instrument>().GetQueryAsNoTracking(Q => Q.QuarantineModel.Select(s => s.StatusId).FirstOrDefault() == 2 && (Q.CreatedBy == userId || Q.UserDept==labUserById.DepartmentId)).Include(I => I.QuarantineModel).Include(I => I.FileUploadModel).Include(G => G.DepartmenttModel).ToList();
-            if(InstrumentList!=null)
-            {
-                ViewBag.InstrumentCount=InstrumentList.Count;
-            }
-            else
-            {
-                ViewBag.InstrumentCount=0;
-            }
-        }
-        if(userRoleId==2 || userRoleId==4 || userRoleId==3)
-        {
-            if(userRoleId==3){
-                List<ExternalRequest> RequestList = _unitOfWork.Repository<ExternalRequest>().GetQueryAsNoTracking().Include(I=>I.MasterModel).Include(I=>I.ExternalRequestStatusModal).Include(I=>I.ExternalRequestStatusModal).ToList();
-                if(RequestList!=null)
-                {
-                    ViewBag.RequestCount=RequestList.Count;
-                }
-                else
-                {
-                    ViewBag.RequestCount=0;
-                }
-            }
-            else
-            {
-                List<Request> RequestList = _unitOfWork.Repository<Request>().GetQueryAsNoTracking().Include(I => I.InstrumentModel).Include(I => I.RequestStatusModel).ToList();
-                if(RequestList!=null)
-                {
-                    ViewBag.RequestCount=RequestList.Count;
-                }
-                else
-                {
-                    ViewBag.RequestCount=0;
-                }
-            }
-        }
-        else{
-            List<Request> RequestList = _unitOfWork.Repository<Request>().GetQueryAsNoTracking(x=>x.CreatedBy==userId || x.LabL4==userId || x.UserL4==userId).Include(I => I.InstrumentModel).Include(I => I.RequestStatusModel).ToList();
-            if(RequestList!=null)
-            {
-                ViewBag.RequestCount=RequestList.Count;
-            }
-            else
-            {
-                ViewBag.RequestCount=0;
-            }
-        }
-        return View();
-    }
+	public IActionResult Index()
+	{
+		ViewBag.PageTitle = "DashBoard";
+		int userId = Convert.ToInt32(base.SessionGetString("LoggedId"));
+		int userRoleId = Convert.ToInt32(base.SessionGetString("UserRoleId"));
+		UserViewModel labUserById = _mapper.Map<UserViewModel>(_unitOfWork.Repository<User>().GetQueryAsNoTracking(Q => Q.Id == userId).SingleOrDefault());
+		List<Master> MasterList = _unitOfWork.Repository<Master>().GetQueryAsNoTracking(g => g.QuarantineModel.Select(s => s.StatusId).FirstOrDefault() == 2).ToList();
+		if (MasterList != null)
+		{
+			ViewBag.MasterCount = MasterList.Count;
+		}
+		else
+		{
+			ViewBag.MasterCount = 0;
+		}
+		if (userRoleId == 2 || userRoleId == 4)
+		{
+			List<Instrument> InstrumentList = _unitOfWork.Repository<Instrument>().GetQueryAsNoTracking(Q => Q.QuarantineModel.Select(s => s.StatusId).FirstOrDefault() == 2 && (Q.IdNo != "" && Q.IdNo != null)).Include(I => I.QuarantineModel).Include(I => I.FileUploadModel).Include(I => I.RequestModel).Include(G => G.DepartmenttModel).ToList();
+			if (InstrumentList != null)
+			{
+				ViewBag.InstrumentCount = InstrumentList.Count;
+			}
+			else
+			{
+				ViewBag.InstrumentCount = 0;
+			}
+		}
+		else
+		{
+			List<Instrument> InstrumentList = _unitOfWork.Repository<Instrument>().GetQueryAsNoTracking(Q => Q.QuarantineModel.Select(s => s.StatusId).FirstOrDefault() == 2 && (Q.CreatedBy == userId || Q.UserDept == labUserById.DepartmentId)).Include(I => I.QuarantineModel).Include(I => I.FileUploadModel).Include(G => G.DepartmenttModel).ToList();
+			if (InstrumentList != null)
+			{
+				ViewBag.InstrumentCount = InstrumentList.Count;
+			}
+			else
+			{
+				ViewBag.InstrumentCount = 0;
+			}
+		}
+		if (userRoleId == 2 || userRoleId == 4 || userRoleId == 3)
+		{
+			if (userRoleId == 3)
+			{
+				List<ExternalRequest> RequestList = _unitOfWork.Repository<ExternalRequest>().GetQueryAsNoTracking().Include(I => I.MasterModel).Include(I => I.ExternalRequestStatusModal).Include(I => I.ExternalRequestStatusModal).ToList();
+				if (RequestList != null)
+				{
+					ViewBag.RequestCount = RequestList.Count;
+				}
+				else
+				{
+					ViewBag.RequestCount = 0;
+				}
+			}
+			else
+			{
+				List<Request> RequestList = _unitOfWork.Repository<Request>().GetQueryAsNoTracking().Include(I => I.InstrumentModel).Include(I => I.RequestStatusModel).ToList();
+				if (RequestList != null)
+				{
+					ViewBag.RequestCount = RequestList.Count;
+				}
+				else
+				{
+					ViewBag.RequestCount = 0;
+				}
+			}
+		}
+		else
+		{
+			List<Request> RequestList = _unitOfWork.Repository<Request>().GetQueryAsNoTracking(x => x.CreatedBy == userId || x.LabL4 == userId || x.UserL4 == userId).Include(I => I.InstrumentModel).Include(I => I.RequestStatusModel).ToList();
+			if (RequestList != null)
+			{
+				ViewBag.RequestCount = RequestList.Count;
+			}
+			else
+			{
+				ViewBag.RequestCount = 0;
+			}
+		}
+		return View();
+	}
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+	public IActionResult Privacy()
+	{
+		return View();
+	}
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 
 	public IActionResult HomePage()
 	{
@@ -123,5 +126,32 @@ public class HomeController : BaseController
 		var Path = _configuration["HomePage"];
 
 		return Redirect(Path);
+	}
+	public IActionResult MasterTranslate()
+	{
+		List<Master> MasterList = _unitOfWork.Repository<Master>().GetQueryAsNoTracking(g => g.QuarantineModel.Select(s => s.StatusId).FirstOrDefault() == 2).ToList();
+		//MasterLangTranslate MStranslater = new MasterLangTranslate();
+		var MStranslater = new List<MasterLangTranslate>();
+		foreach (var item in MasterList)
+		{
+
+			MStranslater.Add(new MasterLangTranslate
+			{
+				id = item.Id,
+				NameEng = item.EquipName,
+				NameJp = item.EquipNameJP,
+
+			});
+		}
+		return Json(MStranslater);
+	}
+
+	public class MasterLangTranslate
+	{
+		public int id { get; set; }
+		public string? NameEng { get; set; }
+
+		public string? NameJp { get; set; }
+
 	}
 }
--- WEB/Controllers/UserController.cs
+++ WEB/Controllers/UserController.cs
@@ -14,250 +14,250 @@ namespace WEB.Controllers;
 
 public class UserController : BaseController
 {
-    private IUserService _userService { get; set; }
-    private IUnitOfWork _unitOfWork { get; set; }
-    private readonly IMapper _mapper;
+	private IUserService _userService { get; set; }
+	private IUnitOfWork _unitOfWork { get; set; }
+	private readonly IMapper _mapper;
 	private IConfiguration _configuration;
 	public UserController(IUserService userService, IMapper mapper, IUnitOfWork unitOfWork, ILogger<BaseController> logger, IHttpContextAccessor contextAccessor, IConfiguration configuration) : base(logger, contextAccessor)
-    {
-        _userService = userService;
-        _unitOfWork = unitOfWork;
-        _mapper = mapper;
+	{
+		_userService = userService;
+		_unitOfWork = unitOfWork;
+		_mapper = mapper;
 		_configuration = configuration;
 	}
 
-    public IActionResult Index()
-    {
-        ViewBag.PageTitle = "User List";
-        ViewBag.ResponseCode = TempData["ResponseCode"];
-        ViewBag.ResponseMessage = TempData["ResponseMessage"];
-        ViewBag.UserDeleteResponse = TempData["UserDeleteResponseData"];
-        ViewBag.UserDeleteFinalResponse = TempData["UserDeleteFinalResponseCode"];
-        ViewBag.DeletingUserIdLast = TempData["DeletingUserId"];
-        ViewBag.DeletingError = TempData["DeletingError"];
-        ViewBag.FinalUserDeleteResponseData = TempData["FinalUserDeleteResponseData"];
+	public IActionResult Index()
+	{
+		ViewBag.PageTitle = "User List";
+		ViewBag.ResponseCode = TempData["ResponseCode"];
+		ViewBag.ResponseMessage = TempData["ResponseMessage"];
+		ViewBag.UserDeleteResponse = TempData["UserDeleteResponseData"];
+		ViewBag.UserDeleteFinalResponse = TempData["UserDeleteFinalResponseCode"];
+		ViewBag.DeletingUserIdLast = TempData["DeletingUserId"];
+		ViewBag.DeletingError = TempData["DeletingError"];
+		ViewBag.FinalUserDeleteResponseData = TempData["FinalUserDeleteResponseData"];
 
-        ResponseViewModel<UserViewModel> response = _userService.GetAllUserList();
-        List<UserViewModel> userById = _mapper.Map<List<UserViewModel>>(_unitOfWork.Repository<User>().GetQueryAsNoTracking(Q => Q.UserRoleId == 2).ToList());
-        response.ResponseDataList.First().Labadminuserlist = userById;
-        return View(response.ResponseDataList);
-    }
+		ResponseViewModel<UserViewModel> response = _userService.GetAllUserList();
+		List<UserViewModel> userById = _mapper.Map<List<UserViewModel>>(_unitOfWork.Repository<User>().GetQueryAsNoTracking(Q => Q.UserRoleId == 2).ToList());
+		response.ResponseDataList.First().Labadminuserlist = userById;
+		return View(response.ResponseDataList);
+	}
 
-    public IActionResult UserEdit(int userId)
-    {
-        ViewBag.PageTitle = "User Edit";
-        ResponseViewModel<UserViewModel> response = _userService.GetUserById(userId);
-        return View("Create", response.ResponseData);
-    }
-    public class CurrentUserInfo
-    {
-        public string UserName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string EmailId { get; set; }
-        public string Landline { get; set; }
-        public string Mobile { get; set; }
-        public int UserCount { get; set; }
-    }
-    public IActionResult GetUserInfoFromLDAP(string ShortId)
-    {
-        var instrumentsList = new List<CurrentUserInfo>();
-        if (!string.IsNullOrEmpty(ShortId) && ShortId.Length > 6)
-        {
-            string domainName = "APAC";
-            DirectorySearcher dssearch = new DirectorySearcher(domainName);
+	public IActionResult UserEdit(int userId)
+	{
+		ViewBag.PageTitle = "User Edit";
+		ResponseViewModel<UserViewModel> response = _userService.GetUserById(userId);
+		return View("Create", response.ResponseData);
+	}
+	public class CurrentUserInfo
+	{
+		public string UserName { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailId { get; set; }
+		public string Landline { get; set; }
+		public string Mobile { get; set; }
+		public int UserCount { get; set; }
+	}
+	public IActionResult GetUserInfoFromLDAP(string ShortId)
+	{
+		var instrumentsList = new List<CurrentUserInfo>();
+		if (!string.IsNullOrEmpty(ShortId) && ShortId.Length > 6)
+		{
+			string domainName = "APAC";
+			DirectorySearcher dssearch = new DirectorySearcher(domainName);
 
-            dssearch.Filter = "(sAMAccountName=" + ShortId + ")";
+			dssearch.Filter = "(sAMAccountName=" + ShortId + ")";
 
-            System.DirectoryServices.SearchResult sresult = dssearch.FindOne();
-            if (sresult != null)
-            {
-                DirectoryEntry dsresult = sresult.GetDirectoryEntry();
-                var currentUserInfo = new CurrentUserInfo
-                {
-                    UserName = dsresult.Properties["SAMAccountName"][0].ToString(),
-                    FirstName = dsresult.Properties["givenName"][0].ToString(),
-                    LastName = dsresult.Properties["sn"][0].ToString(),
-                    EmailId = dsresult.Properties["mail"][0].ToString(),
-                    Landline = dsresult.Properties["telephoneNumber"].Value == null ? "" : dsresult.Properties["telephoneNumber"].Value.ToString(),
-                    Mobile = dsresult.Properties["mobile"].Value == null ? "" : dsresult.Properties["mobile"].Value.ToString()
-                };
-                return Json(currentUserInfo);
-            }
-        }
-        return Json(instrumentsList);
-    }
-    public IActionResult GetUserInfoFromLDAPData(string ShortId, string Level)
-    {
-        User userById = _unitOfWork.Repository<User>().GetQueryAsNoTracking(Q => Q.ShortId == ShortId && Q.Level == Level).SingleOrDefault();
-        return Json(userById);
-    }
-    public IActionResult Create()
-    {
-        ViewBag.PageTitle = "User Create";
-        ResponseViewModel<UserViewModel> response = _userService.CreateNewUser();
-        return View(response.ResponseData);
-    }
-    //public IActionResult ValidateLogin(string userName, string userPassword, string ReturnUrl, string language)
-    public IActionResult ValidateLogin(string email, string ln)
-    {
-	    //language = HttpContext.Request.Query["ln"].ToString();
+			System.DirectoryServices.SearchResult sresult = dssearch.FindOne();
+			if (sresult != null)
+			{
+				DirectoryEntry dsresult = sresult.GetDirectoryEntry();
+				var currentUserInfo = new CurrentUserInfo
+				{
+					UserName = dsresult.Properties["SAMAccountName"][0].ToString(),
+					FirstName = dsresult.Properties["givenName"][0].ToString(),
+					LastName = dsresult.Properties["sn"][0].ToString(),
+					EmailId = dsresult.Properties["mail"][0].ToString(),
+					Landline = dsresult.Properties["telephoneNumber"].Value == null ? "" : dsresult.Properties["telephoneNumber"].Value.ToString(),
+					Mobile = dsresult.Properties["mobile"].Value == null ? "" : dsresult.Properties["mobile"].Value.ToString()
+				};
+				return Json(currentUserInfo);
+			}
+		}
+		return Json(instrumentsList);
+	}
+	public IActionResult GetUserInfoFromLDAPData(string ShortId, string Level)
+	{
+		User userById = _unitOfWork.Repository<User>().GetQueryAsNoTracking(Q => Q.ShortId == ShortId && Q.Level == Level).SingleOrDefault();
+		return Json(userById);
+	}
+	public IActionResult Create()
+	{
+		ViewBag.PageTitle = "User Create";
+		ResponseViewModel<UserViewModel> response = _userService.CreateNewUser();
+		return View(response.ResponseData);
+	}
+	//public IActionResult ValidateLogin(string userName, string userPassword, string ReturnUrl, string language)
+	public IActionResult ValidateLogin(string email, string ln)
+	{
+		//language = HttpContext.Request.Query["ln"].ToString();
 		//ErrorViewModelTest.Log("Email - " + email);
 		//ResponseViewModel<UserViewModel> response = _userService.ValidateUser(userName, userPassword);
 		ResponseViewModel<UserViewModel> response = _userService.ValidateUser(email);
 		//ErrorViewModelTest.Log("response - " + response.ResponseMessage);
 		//ErrorViewModelTest.Log("responseData - " + response.ResponseData);
 		if (response.ResponseMessage == "Success")
-        {
-            if (response.ResponseData != null)
-            {
+		{
+			if (response.ResponseData != null)
+			{
 
-                HttpContext.Session.SetString("UserRoleId", response.ResponseData.UserRoleId.ToString());
-                HttpContext.Session.SetString("FirstName", response.ResponseData.FirstName.ToString());
-                HttpContext.Session.SetString("LastName", response.ResponseData.LastName.ToString());
-                HttpContext.Session.SetString("ShortId", response.ResponseData.ShortId.ToString());
-                HttpContext.Session.SetString("LoggedId", response.ResponseData.Id.ToString());
-                HttpContext.Session.SetString("DepartmentName", response.ResponseData.DepartmentName.ToString());
-                HttpContext.Session.SetString("DepartmentId", response.ResponseData.DepartmentId.ToString());
+				HttpContext.Session.SetString("UserRoleId", response.ResponseData.UserRoleId.ToString());
+				HttpContext.Session.SetString("FirstName", response.ResponseData.FirstName.ToString());
+				HttpContext.Session.SetString("LastName", response.ResponseData.LastName.ToString());
+				HttpContext.Session.SetString("ShortId", response.ResponseData.ShortId.ToString());
+				HttpContext.Session.SetString("LoggedId", response.ResponseData.Id.ToString());
+				HttpContext.Session.SetString("DepartmentName", response.ResponseData.DepartmentName.ToString());
+				HttpContext.Session.SetString("DepartmentId", response.ResponseData.DepartmentId.ToString());
 				HttpContext.Session.SetString("Language", ln.ToString());
 			}
 
-            //if (!string.IsNullOrEmpty(ReturnUrl))
-            //    return Redirect(ReturnUrl);
+			//if (!string.IsNullOrEmpty(ReturnUrl))
+			//    return Redirect(ReturnUrl);
 
-            return RedirectToAction("Index", "Home");
+			return RedirectToAction("Index", "Home");
 
-        }
+		}
 
-        else
-        {
-            TempData["ResponseCode"] = response.ResponseCode;
-            TempData["ResponseMessage"] = response.ResponseMessage;
+		else
+		{
+			TempData["ResponseCode"] = response.ResponseCode;
+			TempData["ResponseMessage"] = response.ResponseMessage;
 
-            return RedirectToAction("Login", "Account");
-        }
-    }
+			return RedirectToAction("Logout", "Account");
+		}
+	}
 
-    public IActionResult SetLanguage(string lang)
-    {
-        HttpContext.Session.SetString("Language", lang);
-        return Json("Success");
-    }
+	public IActionResult SetLanguage(string lang)
+	{
+		HttpContext.Session.SetString("Language", lang);
+		return Json("Success");
+	}
 
 
 	public IActionResult PasswordUpdate(UserViewModel user)
-    {
-        ResponseViewModel<UserViewModel> response;
+	{
+		ResponseViewModel<UserViewModel> response;
 
-        response = _userService.PasswordUpdate(user);
-
-
-        TempData["PasswordUpdate"] = response.ResponseCode;
-        if (response.ResponseMessage == "Success")
-        {
-            return RedirectToAction("Login", "Account");
-        }
-        return RedirectToAction("Login", "Account");
-
-    }
-    public IActionResult InsertUser(UserViewModel user)
-    {
-        ResponseViewModel<UserViewModel> response;
-        if (user.Id != null && user.Id > 0)
-        {
-            user.ModifiedBy = 1;
-            user.ModifiedOn = DateTime.Now;
-            response = _userService.UpdateUser(user);
-        }
-        else
-        {
-            user.ActiveStatus = true;
-            user.CreatedBy = 1;
-            user.ModifiedBy = 1;
-            user.CreatedOn = DateTime.Now;
-            user.ModifiedOn = DateTime.Now;
-            user.ActivationGuid = Guid.NewGuid();
-            response = _userService.InsertUser(user);
-        }
-        TempData["ResponseCode"] = response.ResponseCode;
-        TempData["ResponseMessage"] = response.ResponseMessage;
-        if (response.ResponseMessage == "Success")
-        {
-            return RedirectToAction("Index", "User");
-        }
-        else
-        {
-            return View("Create", response.ResponseData);
-        }
-    }
-    public IActionResult ProfileUpdate()
-    {
-        if (SessionGetString("Id") != null)
-        {
-            int loggedinUserId = Convert.ToInt16(SessionGetString("Id"));
-            ResponseViewModel<UserViewModel> response = _userService.GetUserById(loggedinUserId);
-            return View("ProfileUpdate", response.ResponseData);
-        }
-        else
-        {
-            return RedirectToAction("Login", "Account");
-        }
-    }
-
-    public IActionResult ForgotPassword()
-    {
-        return View();
-    }
-
-    public IActionResult PasswordReset(string UserEmail)
-    {
-        return RedirectToAction("Login", "Account");
-    }
-    public IActionResult UserDelete(int userId)
-    {
-
-        ResponseViewModel<string> response = _userService.DeleteUser(userId);
-
-        TempData["UserDeleteResponseData"] = response.ResponseData;
-        TempData["DeletingUserId"] = userId;
-        return RedirectToAction("Index");
-    }
-    public IActionResult AssignUser(UserViewModel user)
-    {
-        ResponseViewModel<UserViewModel> response;
-
-        if (user.DeletingID != user.Id)
-        {
-            response = _userService.AssignUser(user);
-
-            if (response.ResponseMessage == "Success")
-            {
-
-                ResponseViewModel<string> response1 = _userService.DeleteUser(user.DeletingID);
-                if (response1.ResponseMessage == "Success")
-                {
-                    TempData["FinalUserDeleteResponseData"] = response.ResponseMessage;
-                    TempData["UserDeleteResponseCode"] = response.ResponseCode;
-                    TempData["ResponseMessage"] = response.ResponseMessage;
-                    return RedirectToAction("Index", "User");
-                }
-                else
-                {
-                    TempData["FinalUserDeleteResponseData"] = response.ResponseMessage;
-                    return RedirectToAction("Index", "User");
-                }
+		response = _userService.PasswordUpdate(user);
 
 
-            }
-            else
-            {
-                return RedirectToAction("Index", "User");
-            }
-        }
-        TempData["DeletingError"] = 500;
-        return RedirectToAction("Index", "User");
+		TempData["PasswordUpdate"] = response.ResponseCode;
+		if (response.ResponseMessage == "Success")
+		{
+			return RedirectToAction("Login", "Account");
+		}
+		return RedirectToAction("Login", "Account");
 
-    }
-	
+	}
+	public IActionResult InsertUser(UserViewModel user)
+	{
+		ResponseViewModel<UserViewModel> response;
+		if (user.Id != null && user.Id > 0)
+		{
+			user.ModifiedBy = 1;
+			user.ModifiedOn = DateTime.Now;
+			response = _userService.UpdateUser(user);
+		}
+		else
+		{
+			user.ActiveStatus = true;
+			user.CreatedBy = 1;
+			user.ModifiedBy = 1;
+			user.CreatedOn = DateTime.Now;
+			user.ModifiedOn = DateTime.Now;
+			user.ActivationGuid = Guid.NewGuid();
+			response = _userService.InsertUser(user);
+		}
+		TempData["ResponseCode"] = response.ResponseCode;
+		TempData["ResponseMessage"] = response.ResponseMessage;
+		if (response.ResponseMessage == "Success")
+		{
+			return RedirectToAction("Index", "User");
+		}
+		else
+		{
+			return View("Create", response.ResponseData);
+		}
+	}
+	public IActionResult ProfileUpdate()
+	{
+		if (SessionGetString("Id") != null)
+		{
+			int loggedinUserId = Convert.ToInt16(SessionGetString("Id"));
+			ResponseViewModel<UserViewModel> response = _userService.GetUserById(loggedinUserId);
+			return View("ProfileUpdate", response.ResponseData);
+		}
+		else
+		{
+			return RedirectToAction("Login", "Account");
+		}
+	}
+
+	public IActionResult ForgotPassword()
+	{
+		return View();
+	}
+
+	public IActionResult PasswordReset(string UserEmail)
+	{
+		return RedirectToAction("Login", "Account");
+	}
+	public IActionResult UserDelete(int userId)
+	{
+
+		ResponseViewModel<string> response = _userService.DeleteUser(userId);
+
+		TempData["UserDeleteResponseData"] = response.ResponseData;
+		TempData["DeletingUserId"] = userId;
+		return RedirectToAction("Index");
+	}
+	public IActionResult AssignUser(UserViewModel user)
+	{
+		ResponseViewModel<UserViewModel> response;
+
+		if (user.DeletingID != user.Id)
+		{
+			response = _userService.AssignUser(user);
+
+			if (response.ResponseMessage == "Success")
+			{
+
+				ResponseViewModel<string> response1 = _userService.DeleteUser(user.DeletingID);
+				if (response1.ResponseMessage == "Success")
+				{
+					TempData["FinalUserDeleteResponseData"] = response.ResponseMessage;
+					TempData["UserDeleteResponseCode"] = response.ResponseCode;
+					TempData["ResponseMessage"] = response.ResponseMessage;
+					return RedirectToAction("Index", "User");
+				}
+				else
+				{
+					TempData["FinalUserDeleteResponseData"] = response.ResponseMessage;
+					return RedirectToAction("Index", "User");
+				}
+
+
+			}
+			else
+			{
+				return RedirectToAction("Index", "User");
+			}
+		}
+		TempData["DeletingError"] = 500;
+		return RedirectToAction("Index", "User");
+
+	}
+
 
 }
